3215e2e7ec2d3071286466b0dab6acd9806732f2
Rodrigo Vianna <rvianna@pop-os.localdomain>
2021-10-27 18:59:32 -0300

    menu mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  openMenu: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  menuMobile(event: any) {
+    this.openMenu = !this.openMenu;
+
+    event.target.setAttribute('aria-expanded', this.openMenu)
+
+    if(this.openMenu) {
+      event.target.setAttribute('aria-label', 'Fechar menu');
+    }else {
+      event.target.setAttribute('aria-label', 'Abrir menu')
+    }
+  }
 }
